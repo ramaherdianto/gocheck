fbdac682b6344031f19202fd8514258d4afe78c5
ramaherdianto <ramadhanherdianto35@gmail.com>
2024-07-31 14:43:01 +0700

    feat: create add and delete task

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,106 @@
-const listItems = [
-    {
-        id: 1,
-        title: 'Eat',
-        done: false,
-    },
-    {
-        id: 2,
-        title: 'Sleep',
-        done: true,
-    },
-];
+import { useState } from 'react';
 
 function App() {
+    const [tasks, setTasks] = useState([]);
+    const [inputTask, setInputTask] = useState('');
+    const [showAlert, setShowAlert] = useState(false);
+
+    const onInputTaskChange = (e) => {
+        setInputTask(e.target.value);
+    };
+
+    const submitTaskHandler = (e) => {
+        e.preventDefault();
+        // alert when input value is null
+        if (inputTask.trim() === '') {
+            setShowAlert(true);
+            alert('Input field cannot be empty my dear');
+            return;
+        } else {
+            // Create new task
+            const newTask = {
+                id: new Date().getTime(),
+                task: inputTask,
+                done: false,
+            };
+            setTasks([...tasks, newTask]);
+        }
+        setInputTask('');
+    };
+
+    // handle delete task
+    const deleteTaskHandler = (id) => {
+        const newTask = tasks.filter((item) => item.id !== id);
+        setTasks(newTask);
+    };
+
+    // handle to check task
+    const checkTaskHandler = (id) => {
+        const newTask = tasks.map((item) => {
+            if (item.id === id) {
+                return {
+                    id: item.id,
+                    task: item.task,
+                    done: !item.done,
+                };
+            } else {
+                return item;
+            }
+        });
+        setTasks(newTask);
+    };
+
+    const completedTask = tasks.filter((item) => item.done === true).length;
+    const onGoingTask = tasks.filter((item) => item.done === false).length;
+
     return (
         <div className='app'>
-            <Logo />
-            <Form />
-            <CheckList />
-            <Stats />
+            <span className='logo'>📝 Go Check ✅</span>
+            <form className='add-form' onSubmit={submitTaskHandler}>
+                <input
+                    type='text'
+                    name='inputTask'
+                    value={inputTask}
+                    onChange={onInputTaskChange}
+                />
+                <button>Add</button>
+            </form>
+            <div className='list'>
+                <ul>
+                    {tasks.map((item) => {
+                        return (
+                            <li key={item.id}>
+                                <div>
+                                    <input
+                                        type='checkbox'
+                                        style={{ marginRight: '1em' }}
+                                        onChange={() => checkTaskHandler(item.id)}
+                                    />
+                                    <span
+                                        style={{ textDecoration: item.done ? 'line-through' : '' }}
+                                    >
+                                        {item.task}
+                                    </span>
+                                </div>
+                                <div>
+                                    <button onClick={() => deleteTaskHandler(item.id)}>🗑️</button>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
+                <div className='stats'>
+                    <span>
+                        📝 You have {onGoingTask} notes and{' '}
+                        {completedTask === 0 ? 'none of them' : `only ${completedTask}`} are checked
+                        off (
+                        {tasks.length > 0 ? Math.floor((completedTask / tasks.length) * 100) : 0}%)
+                        ✅
+                    </span>
+                </div>
+            </div>
         </div>
     );
 }
-
-const Logo = () => {
-    return (
-        <>
-            <span className='logo'>📝 Go Check ✅</span>
-        </>
-    );
-};
-const Form = () => {
-    return (
-        <>
-            <div className='add-form'>
-                <h3>Ada yang mau kamu catat?</h3>
-            </div>
-        </>
-    );
-};
-const CheckList = () => {
-    return (
-        <>
-            <div className='list'>
-                <ul>
-                    {listItems.map((item) => {
-                        return <List key={item.id} item={item} />;
-                    })}
-                </ul>
-            </div>
-        </>
-    );
-};
-
-const List = ({ item }) => {
-    return (
-        <>
-            <li>
-                <input type='checkbox' />
-                <span style={{ textDecoration: item.done ? 'line-through' : '' }}>
-                    {item.title}
-                </span>
-                <button>❌</button>
-            </li>
-        </>
-    );
-};
-
-const Stats = () => {
-    return (
-        <>
-            <footer className='stats'>
-                <span>📝 Kamu punya x catatan dan baru x yang dichecklist (x%) ✅</span>
-            </footer>
-        </>
-    );
-};
 
 export default App;
